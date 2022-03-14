5f13d81e8b19471072e48542c42c57eda4981297
Imran Khalid <imran.khalid.code1@gmail.com>
2022-03-14 07:51:38 +0000

    swicth case implemented

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+const App = () => {
+	const [userChoice, setUserChoice] = useState(null);
+	const [compChoice, setCompChoice] = useState(null);
+	const [result, setResult] = useState(null);
+	const choices = ['rock', 'paper', 'scissors'];
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+	const handleClick = (value) => {
+		setUserChoice(value);
+		compScore();
+	};
+
+	const compScore = () => {
+		const random = choices[Math.floor(Math.random() * choices.length)];
+		setCompChoice(random);
+	};
+
+	useEffect(() => {
+		{
+			switch (userChoice + compChoice) {
+				case 'scissorspaper':
+				case 'rockscissors':
+				case 'paperrock':
+					setResult('You Win!');
+					break;
+				case 'paperscissors':
+				case 'scissorsrock':
+				case 'rockpaper':
+					setResult('You Loose');
+					break;
+				case 'paperpaper':
+				case 'scissorsscissors':
+				case 'rockrock':
+					setResult('You Draw');
+					break;
+			}
+		}
+	}, [compChoice, userChoice]);
+
+	return (
+		<div>
+			<h1>User Choice : {userChoice} </h1>
+			<h1>Computer Choice: {compChoice} </h1>
+			<h1>Result:{result}</h1>
+
+			{/* <button onClick={() => handleClick('rock')}>Rock</button>
+			<button onClick={() => handleClick('paper')}>Paper</button>
+			<button onClick={() => handleClick('scissors')}>Scissors</button> */}
+			{choices.map((choice, index) => (
+				<button key={index} onClick={() => handleClick(choice)}>
+					{choice}
+				</button>
+			))}
+		</div>
+	);
+};
 
 export default App;
+
+//extension - add timeout for response.
